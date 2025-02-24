e75ab28029b42880b23be064bbd124314861f786
amorris <amorris@amorris.cc>
2025-02-24 15:41:06 -0500

    Update Apollo Client URI to point to the production server

--- nomadnotes-frontend/src/apollo-client.ts
+++ nomadnotes-frontend/src/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-  uri: "http://127.0.0.1:4000/graphql", // Connects to your Rust API
+  uri: "http://nn.amorris.cc/graphql", // Connects to your Rust API
   cache: new InMemoryCache(), // Caches GraphQL responses
 });
 
