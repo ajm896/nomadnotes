3054b3535d2760ea6d95587645deb32940c86167
Albert Morris <ajm896@gmail.com>
2025-02-26 11:21:48 -0500

    Update GraphQL API URI to use HTTPS

--- nomadnotes-frontend/src/apollo-client.ts
+++ nomadnotes-frontend/src/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
-  uri: "http://nn.amorris.cc/graphql", // Connects to your Rust API
+  uri: "https://nn.amorris.cc/graphql", // Connects to your Rust API
   cache: new InMemoryCache(), // Caches GraphQL responses
 });
 
